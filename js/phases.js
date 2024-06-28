fa3ccda7a18e8a4e4a5de40ea47efb9910c252ce
Kevin <kevhoang97@gmail.com>
2024-06-28 10:56:09 -0400

    pulling the data from local storage

--- js/phases.js
+++ js/phases.js
@@ -2,19 +2,26 @@ const appId = 'dde7350d-cc80-4a05-abd8-f9b38d47f0f4';
 const appSecret = '6509d5ba73f09bc90884cbdde850b5884167e2fe0c7ce02e8aa60592ee8b80b15e606316c02a21ef04206d048aeccffffd5b937004c199111720042b90a62990ffa480ac7aab0102d31a9a253d3d0e87b4c0667756791ad88428fe4b4074915269205c39d493bee8ded062334449e870';
 const backButtonEl = document.querySelector('#back');
 
+const storedLocation = JSON.parse(localStorage.getItem('locationInfo'));
+console.log(storedLocation)
+
+let latitudeData = Number(storedLocation.latitude);
+let longitudeData = Number(storedLocation.longitude);
+let dateData = storedLocation.date
+
 async function getMoonPhases() {
     const url = 'https://api.astronomyapi.com/api/v2/studio/moon-phase';
 
     try {
         const response = await fetch(url, {
-            method: 'GET', 
+            method: 'GET',
             headers: {
                 'Authorization': `Basic ${btoa(`${appId}:${appSecret}`)}`
             }
         });
 
         if (!response.ok) {
-            throw new Error('Network response was not ok'); 
+            throw new Error('Network response was not ok');
         }
 
         const data = await response.json();
@@ -63,7 +70,7 @@ async function getMoonPhaseImage() {
         });
 
         if (!response.ok) {
-            throw new Error('Network response was not ok'); 
+            throw new Error('Network response was not ok');
         }
 
         const data = await response.json();
@@ -84,14 +91,14 @@ async function getNextMoonPhase() {
 
     try {
         const response = await fetch(url, {
-            method: 'GET', 
+            method: 'GET',
             headers: {
                 'Authorization': `Basic ${btoa(`${appId}:${appSecret}`)}`
             }
         });
 
         if (!response.ok) {
-            throw new Error('Network response was not ok'); 
+            throw new Error('Network response was not ok');
         }
 
         const data = await response.json();
@@ -131,7 +138,7 @@ function displayNextMoonPhase(data) {
     const nextPhases = data.data.phenomena.next;
     const nextPhaseContainer = document.getElementById('next-moon-phase-details');
 
-    const nextRelevantPhase = nextPhases.find(phase => 
+    const nextRelevantPhase = nextPhases.find(phase =>
         ['First Quarter', 'Full Moon', 'Last Quarter'].includes(phase.name)
     );
 
@@ -146,7 +153,7 @@ function displayNextMoonPhase(data) {
 }
 
 backButtonEl.addEventListener("click", function (event) {
-location.href = "index.html";
+    location.href = "index.html";
 })
 
 document.getElementById('next-moon-phase-button').addEventListener('click', getNextMoonPhase);
@@ -163,7 +170,7 @@ async function generateStarChart(date, latitude, longitude, style = 'default') {
         observer: {
             latitude: latitude,
             longitude: longitude,
-            date: date
+            date: date,
         },
         view: {
             type: "area",
